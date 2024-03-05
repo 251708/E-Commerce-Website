1cf2ebaaed112e83b0b9269b5675ac5662f515bf
Sakshi Singh <singhsakshigudiya708@gmail.com>
2024-03-05 13:53:28 +0530

    commit

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -105,38 +105,62 @@ const cartReducer = (state, action) => {
     };
   }
 
-  if (action.type === "CART_TOTAL_ITEM") {
-    let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+ /* if (action.type === "CART_TOTAL_ITEM") {
+     let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
       let { amount } = curElem;
 
-      initialVal = initialVal + amount;
-      return initialVal;
-    }, 0);
+       initialVal = initialVal + amount;
+       return initialVal;
+     }, 0);
 
-    return {
-      ...state,
+     return {
+       ...state,
       total_item: updatedItemVal,
     };
-  }
+   }
 
-  if (action.type === "CART_TOTAL_PRICE") {
-    let total_price = state.cart.reduce((initialVal, curElem) => {
-      let { price, amount } = curElem;
+   if (action.type === "CART_TOTAL_PRICE") {
+     let total_price = state.cart.reduce((initialVal, curElem) => {
+       let { price, amount } = curElem;
 
       initialVal = initialVal + price * amount;
-      // 25000 + 0 = 25000
-      // 10199 + 25000 = 121
 
-      return initialVal;
+    return initialVal;
     }, 0);
 
+   return {
+       ...state,
+     total_price,
+    };
+   }
+   */
+
+  if (action.type === "CART_ITEM_PRICE_TOTAL") {
+    let { total_item, total_price } = state.cart.reduce(
+      (accum, curElem) => {
+        let { price, amount } = curElem;
+
+        accum.total_item += amount;
+        accum.total_price += price * amount;
+
+        return accum;
+      },
+      {
+        total_item: 0,
+        total_price: 0,
+      }
+    );
     return {
       ...state,
+      total_item,
       total_price,
     };
   }
+  
 
   return state;
 };
 
 export default cartReducer;
+
+// https://stackoverflow.com/questions/63117470/how-to-return-two-values-in-reduce#:~:text=You%20cannot%20return%20two%20values%20in%20reduce%20.
