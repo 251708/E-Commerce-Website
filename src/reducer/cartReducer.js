0347897322cf7f419129a12ba9830710d22d663d
Sakshi Singh <singhsakshigudiya708@gmail.com>
2024-03-06 20:44:21 +0530

    commit

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,4 +1,4 @@
-const cartReducer = (state, action) => {
+/*const cartReducer = (state, action) => {
   if (action.type === "ADD_TO_CART") {
     let { id, color, amount, product } = action.payload;
 
@@ -104,10 +104,38 @@ const cartReducer = (state, action) => {
       cart: [],
     };
   }
+  if (action.type === "CART_TOTAL_ITEM") {
+       let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+         let { amount } = curElem;    
+          initialVal = initialVal + amount;    
+          return initialVal;
+     }, 0);
+  
+      return {
+       ...state,
+       total_item: updatedItemVal,
+     };
+   }
+  
+   if (action.type === "CART_TOTAL_PRICE") {
+     let total_price = state.cart.reduce((initialVal, curElem) => {
+       let { price, amount } = curElem;
+  
+       initialVal = initialVal + price * amount;
+  
+       return initialVal;
+     }, 0);
+  
+     return {
+       ...state,
+       total_price,
+     };
+   }
+  
 
  
 
-  if (action.type === "CART_ITEM_PRICE_TOTAL") {
+ /*if (action.type === "CART_ITEM_PRICE_TOTAL") {
     let { total_item, total_price } = state.cart.reduce(
       (accum, curElem) => {
         let { price, amount } = curElem;
@@ -117,18 +145,66 @@ const cartReducer = (state, action) => {
 
         return accum;
       },
+      
       {
         total_item: 0,
         total_price: 0,
       }
+      
     );
+    
     return {
       ...state,
       total_item,
       total_price,
     };
+    *
+    
+    
   }
-  
+
+
+  return state;
+};
+
+
+export default cartReducer;
+*/
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_TO_CART") {
+    let { id, color, amount, product } = action.payload;
+    // console.log(
+    //   "🚀 ~ file: cartReducer.js ~ line 4 ~ cartReducer ~ product",
+    //   product
+    // );
+
+    let cartProduct;
+
+    cartProduct = {
+      id: id + color,
+      name: product.name,
+      color,
+      amount,
+      image: product.image[0].url,
+      price: product.price,
+      max: product.stock,
+    };
+
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    let updatedCart = state.cart.filter(
+      (curItem) => curItem.id !== action.payload
+    );
+    return {
+      ...state,
+      cart: updatedCart,
+    };
+  }
 
   return state;
 };
